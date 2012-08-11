8453d7ba1ba91486a56661653759bc186508c983
Jonas Söderström <jonas.soderstrom@gmail.com>
2012-08-11 13:04:43 +0200

    Added so you can create new maps

--- maps.php
+++ maps.php
@@ -11,6 +11,16 @@ else {
 
 	mysql_select_db("meeples_se", $con);
 
+	// Save if we have a value
+	$value = $_GET['new']; 
+	if ( $value != null ){
+		if (mysql_query("INSERT INTO  map (name ,size)VALUES ('".$value."',  '30');")){
+			// Update some note, with jquery?
+		} else {
+			echo "Error creating new map: " . mysql_error();			
+		}
+	}
+
 	$result = mysql_query("SELECT * FROM map");
 
 	while($row = mysql_fetch_array($result))
@@ -24,3 +34,7 @@ else {
 }
 
 ?>
+
+<button onclick="self.location='maps.php?new=' + prompt('Please enter name of new map','');">Create new map</button><br>
+
+
